4f19298ecd760ea0276226497f18cbb216b2e74c
Huz <huzskywalker@tutamail.com>
2026-03-10 02:26:21 +0200

    Fixing comments_spec

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /create" do
+  describe "POST /topics/:topic_id/posts/:post_id/comments" do
     it "returns http success" do
-      get "/comments/create"
+
+      user = create(:user)
+      topic = create(:topic, user: user)
+      post_record = create(:post, topic: topic, user: user)
+
+      sign_in user
+
+      post "/topics/#{topic.id}/posts/#{post_record.id}/comments",
+       params: { comment: { body: "Hello!" } },
+        headers: { "Accept" => "text/vnd.turbo-stream.html" }
       expect(response).to have_http_status(:success)
     end
   end
