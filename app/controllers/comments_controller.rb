55b07e215c3b345e53bd9d44cd1a6db0bf990078
Huz <huzskywalker@tutamail.com>
2026-03-13 01:59:53 +0200

    Fixed user can submit nil comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,19 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [ :edit, :update, :destroy ]
   before_action :authenticate_user!
 
- def create
-  @comment = @post.comments.new(comment_params.merge(user: current_user))
+  def create
+    @comment = @post.comments.new(comment_params.merge(user: current_user))
 
-  respond_to do |format|
-    if @comment.save
-      format.turbo_stream
-      format.html { redirect_to topic_post_url(@topic, @post), notice: "Comment was successfully created." }
-    else
-      format.turbo_stream
-      format.html { render :new, status: :unprocessable_entity }
-    end
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+        format.html { redirect_to topic_post_url(@topic, @post), notice: "Comment was successfully created." }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("comment_form", 
+        partial: "comments/form", 
+        locals: { comment: @comment, topic: @topic, post: @post }) }
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
