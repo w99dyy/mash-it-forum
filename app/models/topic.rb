738110a693308786a3a4226a262f7d195452786d
Huz <huzskywalker@tutamail.com>
2026-03-13 02:01:31 +0200

    topic title validations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,12 @@
 class Topic < ApplicationRecord
   belongs_to :user
   has_many :posts, dependent: :destroy
-  validates :title, presence: true
+  validates :title, presence: { message: "cannot be blank!" },
+                    length: { 
+                      minimum: 10, 
+                      maximum: 100,
+                      too_short: "must be at least %{count} characters long",
+                      too_long: "cannot exceed %{count} characters"
+                    }
   acts_as_taggable_on :tags
 end
