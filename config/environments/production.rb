e35fed3ca67452d6326b20a43d273b2510144f4c
Huz <huzskywalker@tutamail.com>
2026-03-14 00:29:36 +0200

    Setting up Email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,12 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: "127.0.0.1",
+  port: 25,
+  domain: "yourdomain.com"
+  }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
