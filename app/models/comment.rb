6a40705efccbae42eb8573f8e0e1df5219eb0d71
Huz <huzskywalker@tutamail.com>
2026-03-26 01:10:46 +0200

    Added notification logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,25 @@
 class Comment < ApplicationRecord
+  after_create :send_email_notification
   belongs_to :post, counter_cache: true
   belongs_to :user
   belongs_to :parent, class_name: "Comment", optional: true
   has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
   has_many_attached :images
   has_rich_text :body
-
   validates :body, presence: true
+
+  def send_email_notification
+    if parent_id.present?
+      parent_author = parent.user
+
+      if parent_author != user
+        CommentMailer.with(comment: self, recipient: parent_author).reply_notification.deliver_now
+      end
+    else
+      post_author = post.user
+      if post_author != user
+        CommentMailer.with(comment: self, recipient: post_author).post_comment_notification.deliver_now
+      end
+    end
+  end
 end
