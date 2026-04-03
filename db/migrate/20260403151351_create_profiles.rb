4adb670c6e98a1095f26c210638a21280bedfc57
Huz <huzskywalker@tutamail.com>
2026-04-04 01:50:18 +0200

    Profile spec, model and migration

---
+++ db/migrate/20260403151351_create_profiles.rb
@@ -0,0 +1,11 @@
+class CreateProfiles < ActiveRecord::Migration[8.1]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.text :bio
+      t.string :avatar
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :profile do
+    user { nil }
+    bio { "MyText" }
+    avatar { "MyString" }
+  end
+end
