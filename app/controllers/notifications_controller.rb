042cf8bb3127c0564eb8e7d17eb2437165ace954
Huz <huzskywalker@tutamail.com>
2026-05-08 20:20:04 +0300

    Added noticed gem for notifications
    Currently added notifications for comments

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,42 @@
+class NotificationsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @notifications = current_user.notifications.order(created_at: :desc).limit(25)
+
+        if params[:mark_as_read] == "true"
+            @notifications.unread.update.all(read_at: Time.current)
+        end
+
+        respond_to do |format|
+            format.html
+            format.json { render json: @notifications.as_json(methods: notification_data) }
+        end
+    end
+
+    def show
+        @notification = current_user.notifications.find(params[:id])
+        @notification.mark_as_read! unless @notification.read?
+
+        redirect_to @notification.to_path
+    end
+
+    def mark_as_read
+        @notification = current_user.notifications.find(params[:id])
+        @notification.update_all(read_at: Time.current)
+
+        respond_to do |format|
+        format.html { redirect_back fallback_location: notifications_path }
+        format.json { render json: { success: true } }
+        end
+    end
+
+    def mark_all_as_read
+        current_user.notifications.unread.update_all(read_at: Time.current)
+
+        respond_to do |format|
+            format.html { redirect_back fallback_location: notifications_path }
+            format.json { render json: { success: true } }
+        end
+    end
+end
---
+++ app/notifiers/new_comment_notifier.rb
@@ -0,0 +1,51 @@
+# To deliver this notification:
+#
+# NewCommentNotifier.with(record: @post, message: "New post").deliver(User.all)
+
+class NewCommentNotifier < ApplicationNotifier
+  # Add your delivery methods
+  #
+  # deliver_by :email do |config|
+  #   config.mailer = "UserMailer"
+  #   config.method = "new_post"
+  # end
+  #
+  # bulk_deliver_by :slack do |config|
+  #   config.url = -> { Rails.application.credentials.slack_webhook_url }
+  # end
+  #
+  # deliver_by :custom do |config|
+  #   config.class = "MyDeliveryMethod"
+  # end
+
+  # Add required params
+  #
+  # required_param :message
+
+  # Compute recipients without having to pass them in
+  #
+  # recipients do
+  #   params[:record].thread.all_authors
+  # end
+
+  deliver_by :action_cable do |config|
+    config.message = ->(notification) { message }
+  end
+
+  notification_methods do
+    def message
+      "#{params[:comment].user.username} commented on your post!"
+    end
+    def actor
+      params[:comment].user
+    end
+
+    def comment
+      params[:comment]
+    end
+
+    def action
+      "post_comment"
+    end
+  end
+end
