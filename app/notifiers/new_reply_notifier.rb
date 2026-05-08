8e1fb6d7e8fe25b77e77599563d0598efeeeedde
Huz <huzskywalker@tutamail.com>
2026-05-09 02:46:26 +0300

    Create new_reply_notifier.rb

---
+++ app/notifiers/new_reply_notifier.rb
@@ -0,0 +1,48 @@
+# To deliver this notification:
+#
+# NewReplyNotifier.with(record: @post, message: "New post").deliver(User.all)
+
+class NewReplyNotifier < ApplicationNotifier
+  # Add your delivery methods
+  #
+  # deliver_by :email do |config|
+  #   config.mailer = "UserMailer"
+  #   config.method = "new_post"
+  # end
+  #
+  # bulk_deliver_by :slack do |config|
+  #   config.url = -> { Rails.application.credentials.slack_webhook_url }
+  # end
+  #
+  # deliver_by :custom do |config|
+  #   config.class = "MyDeliveryMethod"
+  # end
+
+  # Add required params
+  #
+  # required_param :message
+
+  # Compute recipients without having to pass them in
+  #
+  # recipients do
+  #   params[:record].thread.all_authors
+  # end
+
+  deliver_by :action_cable do |config|
+    config.message = ->(notification) { notification.message }
+  end
+
+  notification_methods do
+    def actor
+      params[:comment].user
+    end
+
+    def comment
+      params[:comment]
+    end
+
+    def action
+      "reply"
+    end
+  end
+end
