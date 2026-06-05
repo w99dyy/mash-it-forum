30dd852cd987af68b33563280657f22e18141c10
Huz <huzskywalker@tutamail.com>
2026-06-05 03:02:48 +0300

    Changed writing to primary instead of queue for neon db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.solid_queue.connects_to = { database: { writing: :primary } }  # default queue
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {
