2330b2405d318b68c836ecb1354d5185b0de421b
Huz <huzskywalker@tutamail.com>
2026-06-07 23:08:56 +0300

    Configure cloudinary for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
+  config.active_storage.service = :cloudinary
+  
   config.action_mailer.perform_deliveries = true
 
   # Don't care if the mailer can't send.
