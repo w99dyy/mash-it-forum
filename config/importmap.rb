ff94b8f9b198369331e2fcce8f5baee03464eea4
Huz <huzskywalker@tutamail.com>
2026-06-07 23:10:30 +0300

    Add activestorage pin

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix" # @2.1.18
+pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "@rails/actiontext", to: "@rails--actiontext.js" # @8.1.300
