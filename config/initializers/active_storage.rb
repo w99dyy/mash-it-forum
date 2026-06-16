66d31c44ababd1b8d3487478fdf2b244d0f4a576
Huz <huzskywalker@tutamail.com>
2026-06-17 00:09:30 +0300

    Created initializer to reduce image uploading timeout

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,5 @@
+Rails.application.config.active_storage.analyzers = []
+Rails.application.config.active_storage.attachment_analyzer = nil
+Rails.application.config.active_storage.image_analyzer = nil
+Rails.application.config.active_storage.video_analyzer = nil
+Rails.application.config.active_storage.variant_processor = :vips
